02c4bc9f7d4d7011a919abc390b02acf669e743e
Hana <hanalways@gmail.com>
2019-05-10 08:38:23 -0700

    auth_callback log in test passing

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,19 @@
 require "test_helper"
 
 describe UsersController do
+  describe "auth_callback" do 
+    it "logs in an existing user and redirects to the root route" do 
+      start_count = User.count 
+      user = users(:dan)
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+
+      get auth_callback_path(:github)
+      must_redirect_to root_path 
+
+      session[:user_id].must_equal user.id 
+
+      User.count.must_equal start_count 
+    end
+  end
 end
